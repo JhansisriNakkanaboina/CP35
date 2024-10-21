e36ce10732020afe0a6944cb38c557b318d43ac4
Jhansisri <njhansisri@gmail.com>
2024-10-21 22:26:55 +0530

    solution1

--- src/main/java/com/csinTelugu/project1/Project1Application.java
+++ src/main/java/com/csinTelugu/project1/Project1Application.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//This is a comment
 @SpringBootApplication
 public class Project1Application {
 
